719b67997b3122d422dbfbc9a775d7faa8e9c22b
newcomer-atcoder <266232001+newcomer-atcoder@users.noreply.github.com>
2026-03-18 14:57:04 +0900

    DBに関するオプション機能の実装(#5)
    - 仕様変更後のオプション機能1~3を実装しpytest完了
    
    - main.py実行して動作確認

--- src/main.py
+++ src/main.py
@@ -1,50 +1,55 @@
 # -*- coding: utf-8 -*-
 
 #自作モジュール
-from my_modules.userif import select_file_in_dialog
+from my_modules.db.settings import DBSetUp
+from my_modules.internal_code.insert import EnterCMD
 from my_modules.facade import AIFacade
 
 #定数(会話プロンプト)
 HELLO = "こんにちは、あなたのランニングを支援します"
-PLEASE_SELECT_FILE = "最初に、学習用データ(Csv)を選択してください (y/n):"
-SELECT_FILE = "y"
-FILE_IS_SELECTED = "学習用データ(Csv)の取得に成功しました!"
-FILE_IS_NOT_SELECTED = "学習用データ(Csv)を取得できませんでした。\nアプリを再起動して、学習用データファイルを再度選択してください。"
-EXIT_AI = "AIを終了します"
-PLEASE_INPUT_DATAS = "\n「走行予定の距離(km)」「体調(%)」を、半角スペース区切りで入力してください\n(入力を終了する場合は「q」を入力):"
+ASK_YOU_INPUT_YOUR_RUNDATA = "AIに過去のランニング記録を入力しますか (y/n):"
+INPUT_MY_RUNDATA = "y"
+YOU_MUST_INPUT_YOUR_RUNDATA = "DBの初回セットアップが完了しました\n続けてAIに過去のランニング記録を入力してください"
+PLEASE_INPUT_DISTANCE_AND_CONDITION = "\n「走行予定の距離(km)」「体調(%)」を、半角スペース区切りで入力してください\n(入力を終了する場合は「q」を入力):"
 EXIT_CMD = "q"
+EXIT_AI = "AIを終了します"
 NAN_MSG = "数値ではない値が入力されました。あるいは入力が不足しています。再入力してください。"
 OUTPUT_RUNNING_DISTANCE = "本日のあなたの適正距離(km):"
 
-#その他定数
-NOFILE = ""
-
 
 def main():
-    #学習用データ(Csv)の入力を促す
+    #DBの初回セットアップ(バックグラウンド処理)
+    #DBがテーブルなしの初期状態であればセットアップ
+    dbsetup = DBSetUp()
+    dbsetup_flg = not dbsetup.check_table_exists()
+    if dbsetup_flg:
+        dbsetup.createTable()
+
+    #アプリ起動(ユーザ側に最初に表示)
     print(HELLO)
-    print(PLEASE_SELECT_FILE)
-    
-    filePass = NOFILE
-    if input() == SELECT_FILE:
-        filePass = select_file_in_dialog()
-    
-    if filePass == NOFILE:
-        print(FILE_IS_NOT_SELECTED)
-        print(EXIT_AI)
-        return
+    if not dbsetup_flg:
+        print(ASK_YOU_INPUT_YOUR_RUNDATA)
+        isInput_my_rundata = input() == INPUT_MY_RUNDATA
     else:
-        print(FILE_IS_SELECTED)
+        #DBセットアップ時はランニング記録の入力必須
+        print(YOU_MUST_INPUT_YOUR_RUNDATA)
+        isInput_my_rundata = True
+    
+    if isInput_my_rundata:
+        cmd_mode = EnterCMD(dbsetup_flg)
+        cmd_mode.input_runData()
+        if cmd_mode.checkedValueList:
+            cmd_mode.insert_into_db()
     
     #AI処理のファサード(窓口)を生成し、AIに学習用データ(Csv)を機械学習させる
-    aiFacade = AIFacade(filePass)
+    aiFacade = AIFacade()
     aiFacade.load_TrainingData()
     aiFacade.trainingDone()
     
     #ユーザー入力、ファサード(窓口)に処理依頼、推論値出力
     while 1:
         try:
-            print(PLEASE_INPUT_DATAS)
+            print(PLEASE_INPUT_DISTANCE_AND_CONDITION)
             input_datas = input()
             if input_datas == EXIT_CMD:
                 print(EXIT_AI)
