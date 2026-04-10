428a224c004ebdcf0c0d7c87e1d9d12bca764ff0
newcomer-atcoder <266232001+newcomer-atcoder@users.noreply.github.com>
2026-04-10 10:58:45 +0900

    テストコード修正 41ebb84のentry.pyの修正に対応

--- test/test_entry.py
+++ test/test_entry.py
@@ -2,44 +2,9 @@ from sqlalchemy import create_engine, select, Integer, Float, Date, CheckConstra
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 from pathlib import Path
 
-from src.my_modules.data.entry import EnterCMD, ValueCheck
-from src.my_modules.data import entry
+from src.my_modules.data.entry import EntryRunData, ValueCheck
 
 import datetime
-import sys
-import io
-
-#inputを制御
-_INPUT = """\
-q
-2026/03/18 5 70 6
-26/4/12 6 50 5
-2026/03/19 5 70
-2026/03/19 5 70 hogehoge
-q
-"""
-sys.stdin = io.StringIO(_INPUT)
-COLUMNS = 4
-DATELEN = 10
-
-#pytest -sオプション推奨、どの例外処理に入ったか確認するため
-def test_input_runData():
-    testInsert = EnterCMD(True)
-    testInsert.input_runData()
-
-    #期待値
-    exp_values = [
-        [datetime.date(2026, 3, 18), 5.0, 70.0, 6.0],
-        [None, 6.0, 50.0, 5.0]
-    ]
-
-    #結果
-    DATE_INDEX, DISTANCE_INDEX, CONDITION_INDEX, RUNNINGDIST_INDEX = 0, 1, 2, 3
-    for exp_value, checkedValues in zip(exp_values, testInsert.checkedValueList):
-        assert exp_value[DATE_INDEX] == checkedValues.date
-        assert exp_value[DISTANCE_INDEX] == checkedValues.distance
-        assert exp_value[CONDITION_INDEX] == checkedValues.condition
-        assert exp_value[RUNNINGDIST_INDEX] == checkedValues.runningDist
 
 #insert_into_db
 #環境構築
@@ -95,12 +60,12 @@ def test_insert_into_db(monkeypatch):
     if not DB_PATH.exists():
         Base.metadata.create_all(engine)
     
-    testInsert = EnterCMD(True)
+    testInsert = EntryRunData()
 
     #期待値
     exp_values = [
-        ValueCheck(date=datetime.date(2026, 3, 18), distance=5.0, condition=70.0, runningDist=6.0),
-        ValueCheck(date=datetime.date(2026, 4, 30), distance=11.0, condition=70.0, runningDist=11.0)
+        ValueCheck(yyyy=2026, mm=3, dd=18, distance=5.0, condition=70.0, runningDist=6.0),
+        ValueCheck(yyyy=2026, mm=4, dd=30, distance=11.0, condition=70.0, runningDist=11.0)
     ]
 
     #結果
@@ -112,7 +77,7 @@ def test_insert_into_db(monkeypatch):
         stmt = select(MocRunDist)
         results = session.scalars(statement=stmt)
         for result, exp_value in zip(results, exp_values):
-            assert result.date == exp_value.date
+            assert result.date == datetime.date(exp_value.yyyy, exp_value.mm, exp_value.dd)
             assert result.distance == exp_value.distance
             assert result.condition == exp_value.condition
             assert result.runningDist == exp_value.runningDist
